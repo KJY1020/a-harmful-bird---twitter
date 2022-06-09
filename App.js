2bf9f3689c6f3e083205906e221e5139a7bb4cb5
TXMAY <47449096+TXMAY@users.noreply.github.com>
2022-06-09 10:33:06 +0900

    twitter-js-first

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import Widgets from "./Widgets";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>yee</h1>
+      <Sidebar />
+      <Feed />
+      <Widgets />
+    </div>
+  );
+}
+
+export default App;
