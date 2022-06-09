a349ee15234e23f26cbb38ac92eeaa8503d71c53
TXMAY <47449096+TXMAY@users.noreply.github.com>
2022-06-09 11:42:46 +0900

    twitter-js-first

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKaTt_AVTaQWJIstNXKaOrHv_lHsaNAn4",
+  authDomain: "twitter-clone-ef818.firebaseapp.com",
+  projectId: "twitter-clone-ef818",
+  storageBucket: "twitter-clone-ef818.appspot.com",
+  messagingSenderId: "516576312387",
+  appId: "1:516576312387:web:bc220d68ace04ea3a9a8f6",
+  measurementId: "G-XERQYTGQMV",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
