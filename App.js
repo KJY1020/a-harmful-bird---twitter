025052a2de8333b3542d60be7fc96416710522a0
TXMAY <47449096+TXMAY@users.noreply.github.com>
2022-06-09 11:42:10 +0900

    twitter-js-second

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>yee</h1>
       <Sidebar />
       <Feed />
       <Widgets />
--- Post.js
+++ Post.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
-import VeriedUserIcon from "@material-ui/icons/VerifiedUser";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
 function Post({ displayName, username, verified, text, image, avatar }) {
   return (
@@ -14,11 +18,21 @@ function Post({ displayName, username, verified, text, image, avatar }) {
           <div className="post__headerText">
             <h3>
               Rafeh Qazi{" "}
-              <span>
+              <span className="post__headerSpecial">
                 <VerifiedUserIcon className="post__badge" />
               </span>
             </h3>
           </div>
+          <div className="post__headerDescription">
+            <p>도레이재연존버6년쨰다씨발놈ㄷㄹ아</p>
+          </div>
+        </div>
+        <img src="" alt="" />
+        <div className="post__footer">
+          <ChatBubbleOutlineIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
         </div>
       </div>
     </div>
